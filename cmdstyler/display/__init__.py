c97c925e7ea6ba6268053698edbe20969efad849
derphilovic <philovic@outlook.de>
2025-09-27 17:47:17 +0200

    printsplit added, not completely functional

--- cmdstyler/display/__init__.py
+++ cmdstyler/display/__init__.py
@@ -1,12 +1,14 @@
 #modules
 import os
 import cmdstyler as cs
+from textwrap import wrap
 #split screen output
+ts = os.get_terminal_size()
 def split(count: int = 1):
     cs.cursor.clear("system")
-    ts = os.get_terminal_size()
     a = 0
     b = 0
+    global scount
     scount = int(count) + int(1)
     while a < ts.lines:
         line = ""
@@ -17,5 +19,32 @@ def split(count: int = 1):
         b = 0
         print(line)
         a += 1
+    cs.cursor.move(0, 0)
 
-split()
+def printsplit(text: str, side: int = 0):
+    line = ""
+    cs.cursor.clear_line()
+    space = ts.columns / scount - 1
+    textlen = len(text)
+    
+    if textlen <= space:
+        remspace = space - textlen
+        line = text + " " * int(remspace) + "|"
+        print(line, end="")
+        cs.cursor.down()
+        cs.cursor.back(500)
+    else:
+        #line = text[0:int(space)]
+        lines = wrap(text, space)
+        for line in lines:
+            textlen = len(line)
+            remspace = space - textlen
+            line1 = str(line) + " " * int(remspace) + "|"
+            print(line1, end="")
+            cs.cursor.down()
+            cs.cursor.back(500)
+
+split(5)
+printsplit("Halp")
+printsplit("Halp too and I am a spudid guy that writes for fun and has no hobbies. Skibidi toilet is my favorite series, I enjoy watching it very much!")
+cs.cursor.down(70)
